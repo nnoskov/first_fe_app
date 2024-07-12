ea421e5a729559b53b0d8b75566a892a83a6601f
nnoskov <hhb@bk.ru>
2024-07-12 16:30:39 +0300

    Ton connect button has been added to main view.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const manifestUrl =
+  "https://nnoskov.github.io/first_fe_app/tonconnect-manifest.json";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </React.StrictMode>,
-)
+  </TonConnectUIProvider>
+);
